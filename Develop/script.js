fa4dd970740b310234f5d80e2ac34151a391b1a8
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-02-22 21:03:06 -0500

    updating repo

---
+++ Develop/script.js
@@ -0,0 +1,90 @@
+$(".time-div").each(function () {
+    var timeDiv = $(this).attr("id").split("-")[1];
+    
+    if (currentHour == timeDiv) {
+      $(this).addClass("present");
+      $(this).children(".description").addClass("white-text");
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+  });
+  
+  
+  
+  
+  $(function () {
+  $('.saveBtn').on('click', function() {
+    var text = $(this).siblings('.description').val();
+    console.log(text)
+    var time = $(this).parent().attr('id');
+    console.log(time)
+    localStorage.setItem(time, text)
+  })
+  });
+  
+  
+  
+  
+    // TODO: Add a listener for click events on the save button. This code should
+    // use the id in the containing time-block as a key to save the user input in
+    // local storage. HINT: What does `this` reference in the click listener
+    // function? How can DOM traversal be used to get the "hour-x" id of the
+    // time-block containing the button that was clicked? How might the id be
+    // useful when saving the description in local storage?
+    //
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-1 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-2 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-3 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-4 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-5 .description').val(localStorage.getItem('hour-17'));
+  
+  
+  const currentHour = new Date().getHours();
+  
+  const timeBlocks = document.querySelectorAll('.time-block');
+  timeBlocks.forEach(block => {
+    const hour = parseInt(block.id.split('-')[1]);
+    if (hour < currentHour) {
+      block.classList.add('past');
+    } else if (hour === currentHour) {
+      block.classList.add('present');
+    } else {
+      block.classList.add('future');
+    }
+  });
+  
+  
+  // TODO: Add code to display the current date in the header of the page.
+  
+  let currentDate = new Date();
+  let cDay = currentDate.getDate();
+  let cMonth = currentDate.getMonth() + 1;
+  let cYear = currentDate.getFullYear();
+  document.getElementById('currentTime').innerHTML =("<b>" + cMonth + "/" + cDay + "/" + cYear + "</b>");
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
